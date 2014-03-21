30c42fd6ee42cbdf7ee7cbec1673b2259bea23fa
Jonas Gröger <jonas.groeger@gmail.com>
2014-03-22 00:12:23 +0100

    Better project setup.
    We want to have the license be called license.
    Switched from old distutils to much better setuptools.
    Made 'inventory' public in module so that we can have it as a script on the command line (look console_scripts in setup.py).
    Much more comprehensive .gitignore to ignore build packages and distributions etc.

--- setup.py
+++ setup.py
@@ -1,14 +1,37 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
-from distutils.core import setup
+from setuptools import setup
+import codecs
+
+# Get the long description from the relevant file
+with codecs.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name        = 'sllurp',
-    version     = '0.0.1',
+    name = 'sllurp',
+    version = '0.0.1',
     description = 'Python LLRP client',
-    author      = 'Ben Ransford',
+    author = 'Ben Ransford',
     author_email = 'ransford@cs.washington.edu',
-    url         = 'https://github.com/ransford/sllurp',
-    license     = 'GPLv3',
-    packages    = ['sllurp']
+    url = 'https://github.com/ransford/sllurp',
+    license = 'GPLv3',
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',        
+    ],
+    keywords = 'rfid llrpyc reader',
+    packages = ['sllurp'],
+    install_requires = ['twisted'],
+    entry_points = {
+        'console_scripts': [
+            'inventory=sllurp.inventory:main',
+        ],
+    },
 )
