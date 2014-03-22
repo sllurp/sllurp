9e663b088f7fd701df5b2c6b7a5be4a9b01d5155
Jonas Gröger <jonas.groeger@gmail.com>
2014-03-22 02:14:46 +0100

    Read version from file. Refactored description reading.

--- setup.py
+++ setup.py
@@ -1,21 +1,47 @@
 #!/usr/bin/env python2
 
 from setuptools import setup
+import os
+import re
 import codecs
 
-# Get the long description from the relevant file
-with codecs.open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def find_version(*file_paths):
+    """
+    Read the version number from a source file.
+    Why read it, and not import?
+    see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+    """
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'utf-8') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r'^__version__ = [\'"]([^"\']*)["\']', version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+def read(filename):
+    """
+    Get the long description from a file.
+    """
+    with codecs.open(filename, encoding='utf-8') as f:
+        return f.read()
+
 
 setup(
-    name = 'sllurp',
-    version = '0.0.1',
-    description = 'Python LLRP client',
-    author = 'Ben Ransford',
-    author_email = 'ransford@cs.washington.edu',
-    url = 'https://github.com/ransford/sllurp',
-    license = 'GPLv3',
-    classifiers = [
+    name='sllurp',
+    version=find_version('sllurp', '__init__.py'),
+    description=read('README.md'),
+    author='Ben Ransford',
+    author_email='ransford@cs.washington.edu',
+    url='https://github.com/ransford/sllurp',
+    license='GPLv3',
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -23,12 +49,12 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Topic :: Scientific/Engineering :: Information Analysis',        
+        'Topic :: Scientific/Engineering :: Information Analysis',
     ],
-    keywords = 'rfid llrpyc reader',
-    packages = ['sllurp'],
-    install_requires = ['twisted'],
-    entry_points = {
+    keywords='rfid llrpyc reader',
+    packages=['sllurp'],
+    install_requires=['twisted'],
+    entry_points={
         'console_scripts': [
             'inventory=sllurp.inventory:main',
         ],
