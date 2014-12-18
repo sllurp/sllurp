79d069269b6c17664e31ff92c8e2e8d111982480
Ben Ransford <ben@ransford.org>
2014-12-18 15:56:23 -0800

    potentially solve #36 with an extra check

--- sllurp/llrp.py
+++ sllurp/llrp.py
@@ -503,9 +503,15 @@ class LLRPClient (LineReceiver):
 
         while data:
             # parse the message header to grab its length
-            msg_type, msg_len, message_id = \
-                struct.unpack(LLRPMessage.full_hdr_fmt,
-                              data[:LLRPMessage.full_hdr_len])
+            try:
+                msg_type, msg_len, message_id = \
+                    struct.unpack(LLRPMessage.full_hdr_fmt,
+                                  data[:LLRPMessage.full_hdr_len])
+            except struct.error as ae:
+                logger.warning('Too few bytes ({}) to unpack LLRP message' \
+                        'header'.format(len(data)))
+                break
+
             logger.debug('expect {} bytes (have {})'.format(msg_len, len(data)))
 
             if len(data) < msg_len:
