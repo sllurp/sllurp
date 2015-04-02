8f747b56168a5321506f5c9d2c7911cd7bc61cce
Jethro Tan <jethrotan20@gmail.com>
2015-04-02 14:36:44 +0200

    Add support for BlockWrite.

--- sllurp/llrp_proto.py
+++ sllurp/llrp_proto.py
@@ -1591,7 +1591,10 @@ def encode_AccessCommand (par):
     data = encode_C1G2TagSpec(par['TagSpecParameter'])
 
     if 'WriteData' in par['OpSpecParameter']:
-        data += encode_C1G2Write(par['OpSpecParameter'])
+        if par['OpSpecParameter']['WriteDataWordCount'] > 1:
+            data += encode_C1G2BlockWrite(par['OpSpecParameter'])
+        else:
+            data += encode_C1G2Write(par['OpSpecParameter'])
     else:
         data += encode_C1G2Read(par['OpSpecParameter'])
 
@@ -1725,6 +1728,37 @@ def encode_C1G2Write (par):
 
 Message_struct['C1G2Write'] = {
     'type': 342,
+    'fields': [
+        'Type',
+        'OpSpecID',
+        'MB',
+        'WordPtr',
+        'AccessPassword'
+        'WriteDataWordCount',
+        'WriteData'
+    ],
+    'encode': encode_C1G2Write
+}
+
+# 16.2.1.3.2.7 C1G2BlockWrite
+def encode_C1G2BlockWrite (par):
+    msgtype = Message_struct['C1G2BlockWrite']['type']
+    msg_header = '!HH'
+    msg_header_len = struct.calcsize(msg_header)
+
+    data = struct.pack('!H', int(par['OpSpecID']))
+    data += struct.pack('!I', int(par['AccessPassword']))
+    data += struct.pack('!B', int(par['MB']) << 6)
+    data += struct.pack('!H', int(par['WordPtr']))
+    data += struct.pack('!H', int(par['WriteDataWordCount']))
+    data += par['WriteData']
+
+    data = struct.pack(msg_header, msgtype,
+            len(data) + msg_header_len) + data
+    return data
+
+Message_struct['C1G2BlockWrite'] = {
+    'type': 347,
     'fields': [
         'Type',
         'OpSpecID',
