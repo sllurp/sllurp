b2eff7ab608afe3807e556dd4f50e6c8a3438516
Ben Ransford <ben@ransford.org>
2017-05-23 22:00:06 -0700

    remove inventory.parse_args

--- sllurp/verb/inventory.py
+++ sllurp/verb/inventory.py
@@ -57,44 +57,6 @@ def tagReportCallback(llrpMsg):
         numTags += tag['TagSeenCount'][0]
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Simple RFID Inventory')
-    parser.add_argument('host', help='hostname or IP address of RFID reader',
-                        nargs='+')
-    parser.add_argument('-p', '--port', default=llrp.LLRP_PORT, type=int,
-                        help='port (default {})'.format(llrp.LLRP_PORT))
-    parser.add_argument('-t', '--time', type=float,
-                        help='seconds to inventory (default forever)')
-    parser.add_argument('-d', '--debug', action='store_true',
-                        help='show debugging output')
-    parser.add_argument('-n', '--report-every-n-tags', type=int,
-                        dest='every_n', metavar='N',
-                        help='issue a TagReport every N tags')
-    parser.add_argument('-a', '--antennas', default='1',
-                        help='comma-separated list of antennas to use (0=all;'
-                        ' default 1)')
-    parser.add_argument('-X', '--tx-power', default=0, type=int,
-                        dest='tx_power',
-                        help='transmit power (default 0=max power)')
-    mods = sorted(Modulation_Name2Type.keys())
-    parser.add_argument('-M', '--modulation', default=DEFAULT_MODULATION,
-                        choices=mods,
-                        help='modulation (default={})'.format(
-                            DEFAULT_MODULATION))
-    parser.add_argument('-T', '--tari', default=0, type=int,
-                        help='Tari value (default 0=auto)')
-    parser.add_argument('-s', '--session', default=2, type=int,
-                        help='Gen2 session (default 2)')
-    parser.add_argument('-P', '--tag-population', default=4, type=int,
-                        dest='population',
-                        help="Tag Population value (default 4)")
-    parser.add_argument('-l', '--logfile')
-    parser.add_argument('-r', '--reconnect', action='store_true',
-                        default=False,
-                        help='reconnect on connection failure or loss')
-    return parser.parse_args()
-
-
 def main(args):
     # special case default Tari values
     if args.modulation in Modulation_DefaultTari:
