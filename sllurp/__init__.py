d5ea2c735c9e5f9704314e48ff396fbe3d867f20
Ben Ransford <ben@ransford.org>
2017-05-23 21:16:47 -0700

    fix version number in sllurp.__init__

--- sllurp/__init__.py
+++ sllurp/__init__.py
@@ -1,3 +1,2 @@
-__all__ = ('llrp', 'llrp_decoder', 'llrp_errors', 'llrp_proto', 'util',
-           'inventory')
-__version__ = '0.0.1'
+__all__ = ('llrp', 'llrp_decoder', 'llrp_errors', 'llrp_proto', 'util')
+__version__ = '0.1.7'
