b9162abaff009bcc91213a4aa61a079a66b2ce25
Ben Ransford <ben@ransford.org>
2017-06-18 10:48:30 -0700

    'sllurp version' command

--- sllurp/cli.py
+++ sllurp/cli.py
@@ -1,10 +1,11 @@
 """Command-line wrapper for sllurp commands.
 """
 
+from __future__ import print_function
 from collections import namedtuple
 import logging
 import click
-from . import log
+from . import log, __version__
 from .verb import reset as _reset
 from .verb import inventory as _inventory
 from .llrp_proto import Modulation_Name2Type, DEFAULT_MODULATION
@@ -60,6 +61,11 @@ def inventory(host, port, time, report_every_n_tags, antennas, tx_power,
                 reconnect=reconnect)
     logger.debug('inventory args: %s', args)
     _inventory.main(args)
+
+
+@cli.command()
+def version():
+    print(__version__)
 
 
 @cli.command()
