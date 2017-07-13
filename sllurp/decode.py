26dfef52df9ab3db6d3a2eec854a5bc30e8c10fb
Ben Ransford <ben@ransford.org>
2017-07-12 22:22:45 -0700

    sllurp.decoder: use click, support message in file

--- sllurp/decode.py
+++ sllurp/decode.py
@@ -1,38 +1,44 @@
+from __future__ import print_function
 import binascii
-import argparse
 import logging
-from sllurp.llrp import LLRPMessage
+import click
+from . import log
+from .llrp import LLRPMessage
+
 
 logger = logging.getLogger('sllurp')
 
-args = None
+
+def bytes_from_file(path, hex=False):
+    mode = 'r' if hex else 'r'
+    with open(path, mode) as fh:
+        bts = fh.read()
+    return binascii.unhexlify(bts) if hex else bts
 
 
-def parse_args():
-    global args
-    parser = argparse.ArgumentParser(description='Decode an LLRP message')
-    parser.add_argument('msg', help='message in hexadecimal encoding')
-    parser.add_argument('-d', '--debug', action='store_true')
-    args = parser.parse_args()
+@click.command()
+@click.option('-m', '--message', type=str,
+              help='Message in hexadecimal encoding')
+@click.option('-f', '--infile-binary', type=click.Path(exists=True),
+              help='File containing binary message')
+@click.option('-A', '--infile-hex', type=click.Path(exists=True),
+              help='File containing hexadecimal-encoded message')
+@click.option('-d', '--debug', is_flag=True, default=False)
+def main(message, infile_binary, infile_hex, debug):
+    log.init_logging(debug)
+    mbytes = None
+    if message:
+        mbytes = binascii.unhexlify(message)
+    elif infile_binary:
+        mbytes = bytes_from_file(infile_binary, hex=False)
+    elif infile_hex:
+        mbytes = bytes_from_file(infile_hex, hex=True)
 
-
-def init_logging():
-    logLevel = (args.debug and logging.DEBUG or logging.INFO)
-    logFormat = '%(asctime)s %(name)s: %(levelname)s: %(message)s'
-    formatter = logging.Formatter(logFormat)
-    stderr = logging.StreamHandler()
-    stderr.setFormatter(formatter)
-
-    root = logging.getLogger()
-    root.setLevel(logLevel)
-    root.handlers = [stderr]
+    if mbytes:
+        msg = LLRPMessage(msgbytes=mbytes)
+        print('Decoded message:\n==========')
+        print(msg)
 
 
 if __name__ == '__main__':
-    parse_args()
-    init_logging()
-
-    m = binascii.unhexlify(args.msg)
-    msg = LLRPMessage(msgbytes=m)
-    print 'Decoded message:\n=========='
-    print msg
+    main()
