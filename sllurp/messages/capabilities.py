031f0d4fb411c12904e06b13b74918a4eccabb38
Ben Ransford <ben@ransford.org>
2017-07-13 00:16:26 -0700

    INCOMPLETE capabilities message work

--- sllurp/messages/capabilities.py
+++ sllurp/messages/capabilities.py
@@ -19,6 +19,21 @@ LLRPStatus = cs.Struct(
 )
 
 
+GeneralDeviceCapabilities = cs.Struct(
+    cs.Embedded(LLRPParamHeader),
+    'max_number_of_antenna_supported' / cs.Int16ub,
+    'c' / cs.Flag,
+    't' / cs.Flag,
+    cs.Padding(12),
+    'device_manufacturer_name' / cs.Int32ub,
+    'model_name' / cs.Int32ub,
+    'firmware_version_byte_count' / cs.Int16ub,
+    'firmware_version' / cs.String(this.firmware_version_byte_count,
+                                   encoding="utf8"),
+    #'receive_sensitivity_table' /
+)
+
+
 class GetSupportedVersion(LLRPMessage):
     # 17.1.1 GET_SUPPORTED_VERSION
     ty = 46
@@ -34,8 +49,35 @@ class GetSupportedVersionResponse(LLRPMessage):
     )
 
 
+class SetProtocolVersion(LLRPMessage):
+    # 17.1.3 SET_PROTOCOL_VERSION
+    ty = 47
+    struct = cs.Struct(
+        'protocol_version' / cs.Int8ub
+    )
+
+
+class SetProtocolVersionResponse(LLRPMessage):
+    # 17.1.4 GET_SUPPORTED_VERSION_RESPONSE
+    ty = 57
+    struct = cs.Struct(
+        'llrp_status' / LLRPStatus
+    )
+
+
 class GetReaderCapabilities(LLRPMessage):
     ty = 1
     struct = cs.Struct(
         'requested_data' / cs.Int8ub,
+    )
+
+
+class GetReaderCapabilitiesResponse(LLRPMessage):
+    ty = 11
+    struct = cs.Struct(
+        'llrp_status' / LLRPStatus,
+        'general_device_capabilities' / GeneralDeviceCapabilities,
+        #'llrp_capabilities' / LLRPCapabilities,
+        #'regulatory_capabilities' / RegulatoryCapabilities,
+        #'air_protocol_llrp_capabilities' / AirProtocolLLRPCapabilities
     )
