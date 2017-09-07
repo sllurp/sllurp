beb7cad3425ea811931a8fe6d3a4745489d7c558
Ben Ransford <ben@ransford.org>
2017-09-06 17:45:30 -0700

    sllurp.access -> sllurp.verb.access
    also, python -m sllurp.access [args] -> sllurp access [args]

--- sllurp/cli.py
+++ sllurp/cli.py
@@ -10,6 +10,7 @@ from . import log as loggie
 from .verb import reset as _reset
 from .verb import inventory as _inventory
 from .verb import log as _log
+from .verb import access as _access
 from .llrp_proto import Modulation_Name2Type
 
 # Disable Click unicode warning since we use unicode string exclusively
@@ -65,6 +66,7 @@ def inventory(host, port, time, report_every_n_tags, antennas, tx_power,
     _inventory.main(args)
 
 
+@cli.command()
 @click.argument('host', type=str, nargs=-1)
 @click.option('-o', '--outfile', type=click.File('w'), default='-')
 @click.option('-a', '--antennas', type=str, default='0',
@@ -72,9 +74,50 @@ def inventory(host, port, time, report_every_n_tags, antennas, tx_power,
 @click.option('-g', '--stagger', type=int,
               help='delay (ms) between connecting to readers')
 @click.option('-e', '--epc', type=str, help='log only a specific EPC')
-@cli.command()
 def log(host, outfile, antennas, stagger, epc):
     _log.main(host, outfile, antennas, stagger, epc)
+
+
+@cli.command()
+@click.argument('host', type=str, nargs=-1)
+@click.option('-p', '--port', type=int, default=5084)
+@click.option('-t', '--time', type=float, help='seconds to inventory')
+@click.option('-n', '--report-every-n-tags', type=int,
+              help='issue a TagReport every N tags')
+@click.option('-X', '--tx-power', type=int, default=0,
+              help='transmit power (default 0=max power)')
+@click.option('-M', '--modulation', type=click.Choice(mods),
+              help='Reader-to-Tag Modulation')
+@click.option('-T', '--tari', type=int, default=0,
+              help='Tari value (default 0=auto)')
+@click.option('-s', '--session', type=int, default=2,
+              help='Gen2 session (default 2)')
+@click.option('-P', '--tag-population', type=int, default=4,
+              help='Tag Population value (default 4)')
+@click.option('-r', '--read-words', type=int,
+              help='Read N words from tag memory')
+@click.option('-w', '--write-words', type=int,
+              help='Write N words to tag memory')
+@click.option('-mb', '--memory-bank', type=click.IntRange(0, 3),
+              help='Memory bank: 3 User, 2 TID, 1 EPC, 0 Reserved')
+@click.option('-wp', '--word-ptr', type=int,
+              help='Word addresss of the first word to read/write')
+@click.option('-ap', '--access-password', default=0, type=int,
+              help='Access password for secure state if R/W locked')
+def access(host, port, time, report_every_n_tags, tx_power, modulation, tari,
+           session, tag_population, read_words, write_words, memory_bank,
+           word_ptr, access_password):
+    Args = namedtuple('Args', ['host', 'port', 'time', 'every_n',
+                               'tx_power', 'modulation', 'tari', 'session',
+                               'population', 'read_words', 'write_words',
+                               'mb', 'word_ptr', 'access_password'])
+    args = Args(host=host, port=port, time=time, every_n=report_every_n_tags,
+                tx_power=tx_power, modulation=modulation, tari=tari,
+                session=session, population=tag_population,
+                read_words=read_words, write_words=write_words, mb=memory_bank,
+                word_ptr=word_ptr, access_password=access_password)
+    logger.debug('access args: %s', args)
+    _access.main(args)
 
 
 @cli.command()
