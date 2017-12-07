3fa821a8de8cb29df69bf6ec1149cf3e0281dd04
Ben Ransford <ben@ransford.org>
2017-12-07 11:00:47 -0800

    bump version 0.3.8 -> 0.3.9

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_deps = [
 
 setup(
     name='sllurp',
-    version='0.3.8',
+    version='0.3.9',
     description='RFID reader control library',
     long_description=read('README.rst'),
     author='Ben Ransford',
