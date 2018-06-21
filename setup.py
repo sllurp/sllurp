bfd248a29865158c412c0de175483c8cc3aeffa4
Ben Ransford <ben@ransford.org>
2018-06-21 10:47:07 -0700

    clean up pytest running

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     install_requires=install_deps,
     tests_require=test_deps,
     extras_require={'test': test_deps},
-    test_suite='nose2.collector.collector',
+    setup_requires=['pytest-runner'],
     entry_points={
         'console_scripts': [
             'sllurp=sllurp.cli:cli',
