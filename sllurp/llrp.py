bbd73b12abde96e2f3aa58e245aa8e14ba155c32
Florent Viard <fviard@cxignited.com>
2019-01-09 11:33:25 +0100

    Add a minor function as api to update reader llrp config

--- sllurp/llrp.py
+++ sllurp/llrp.py
@@ -1443,6 +1443,9 @@ class LLRPReaderClient(object):
         self._event_notification_callbacks = []
         self._disconnected_callbacks = []
 
+    def update_config(self, new_config):
+        self.config = new_config
+
     def get_peername(self):
         return (self._host, self._port)
 
