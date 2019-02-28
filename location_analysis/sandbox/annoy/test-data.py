ff8968551a1e4dbdb3645c718dc16fb0d9c8170a
Renaud Vincent <renaudv@momentfactory.com>
2019-02-28 15:36:02 -0500

    Adding detected zone in results

--- location_analysis/sandbox/annoy/test-data.py
+++ location_analysis/sandbox/annoy/test-data.py
@@ -22,16 +22,10 @@ results = {}
 
 # Create device index in results dict
 for id in device_ids:
-  results[id] = {}
-  results[id]['pts'] = {}
+  results[id] = {'pts':{}}
 
   for i in range(1, 4):
-    results[id][i] = 1
     results[id]['pts']['zone' + str(i)] = []
-
-
-
-pts = []
 
 for event in history:
   x = event['toX']
@@ -44,22 +38,39 @@ for event in history:
   dist = math.sqrt(dist.pop()) # Squared by lib
   dev_id = event['epc']
 
-  pts += [[x, y]]
-  results[dev_id][closest_zone] += 1
   results[dev_id]['pts']['zone' + str(closest_zone)] += [[x, y]]
 
-centroids = [
-  t.get_item_vector(1)[:2],
-  t.get_item_vector(2)[:2],
-  t.get_item_vector(3)[:2]
-]
 
 for r in results:
-  # plt.scatter(*zip(*results[r]['pts']),s=100, alpha=0.9)
-  for i in range(1, 4):
-    print(len(results[r]['pts']['zone' + str(i)]))
-    if (len(results[r]['pts']['zone' + str(i)])):
-      plt.scatter(*zip(*results[r]['pts']['zone' + str(i)]),s=100, alpha=0.9)
+  zone1_amnt = len(results[r]['pts']['zone1'])
+  zone2_amnt = len(results[r]['pts']['zone2'])
+  zone3_amnt = len(results[r]['pts']['zone3'])
 
+  in_zone1 = zone1_amnt > zone2_amnt and zone1_amnt > zone3_amnt
+  in_zone2 = zone2_amnt > zone1_amnt and zone2_amnt > zone3_amnt
+  in_zone3 = zone3_amnt > zone1_amnt and zone3_amnt > zone2_amnt
 
-plt.show()
+  if in_zone1:
+    results[r]['location'] = 'zone 1'
+  elif in_zone2:
+    results[r]['location'] = 'zone 2'
+  elif in_zone3:
+    results[r]['location'] = 'zone 3'
+
+# Print as Df for better display
+print(pd.DataFrame(results))
+
+# centroids = [
+#   t.get_item_vector(1)[:2],
+#   t.get_item_vector(2)[:2],
+#   t.get_item_vector(3)[:2]
+# ]
+
+# for r in results:
+#   # plt.scatter(*zip(*results[r]['pts']),s=100, alpha=0.9)
+#   for i in range(1, 4):
+#     print(len(results[r]['pts']['zone' + str(i)]))
+#     if (len(results[r]['pts']['zone' + str(i)])):
+#       plt.scatter(*zip(*results[r]['pts']['zone' + str(i)]),s=100, alpha=0.9)
+
+# plt.show()
