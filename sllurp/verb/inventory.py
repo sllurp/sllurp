04a9229c612907acf9d9fffdf985c425c49975a1
Han Zhao <hanzhao>
2020-06-01 16:30:41 -0400

    added connection retry attempts as a config

--- sllurp/verb/inventory.py
+++ sllurp/verb/inventory.py
@@ -59,6 +59,7 @@ def main(args):
         start_inventory=True,
         disconnect_when_done=args.time and args.time > 0,
         reconnect=args.reconnect,
+        reconnect_attempts=args.reconnect_attempts,
         tag_filter_mask=args.tag_filter_mask,
         tag_content_selector={
             'EnableROSpecID': False,
