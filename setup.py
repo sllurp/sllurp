cb8bb4069f26ce4714fc63a67a5eff5b709113a1
Florent Viard <florent@sodria.com>
2023-12-18 18:12:53 +0100

    bump version 0.5.1 -> 0.6.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_deps = [
 
 setup(
     name='sllurp',
-    version='0.5.1',
+    version='0.6.0',
     description='RFID reader control library',
     long_description=read('README.rst'),
     author='Ben Ransford',
