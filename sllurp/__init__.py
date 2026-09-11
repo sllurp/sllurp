2848d53de6b6295318c0a42009fb966ecec5f34b
GuLuLuIt <lini10011001@gmail.com>
2026-09-11 02:45:10 -0400

    Expose management and dedup modules

--- sllurp/__init__.py
+++ sllurp/__init__.py
@@ -1,7 +1,16 @@
-"""Low Level Reader Protocol implemtnation in pure Python"""
+"""Low Level Reader Protocol implementation in pure Python."""
 
 from .version import __version__ as sllurp_version
 
-__all__ = ("llrp", "llrp_decoder", "llrp_errors", "llrp_proto", "util", "log")
+__all__ = (
+    "llrp",
+    "llrp_decoder",
+    "llrp_errors",
+    "llrp_proto",
+    "reader_management",
+    "dedup",
+    "util",
+    "log",
+)
 
 __version__ = sllurp_version
